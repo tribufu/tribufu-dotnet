92dffe0b72b02e321f44a3a3e9a1f94c5d1f5ec0
Guilherme Werner <guilhermewerner@tribufu.com>
2026-05-08 21:48:17 -0300

    Add native bindings lib

---
+++ src/Tribufu.Native/TribufuNative.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Tribufu. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+namespace Tribufu.Native
+{
+    /// <summary>
+    /// Provides methods to interact with the Tribufu native library.
+    /// </summary>
+    public static partial class TribufuNative
+    {
+    }
+}
